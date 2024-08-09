7e78ef8e6835d9ca4ff2571de1420110793609b3
Heroku-Back <143542266+Heroku-Back@users.noreply.github.com>
2024-08-09 18:23:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/z_zzz8")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/z_zzz8")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "True"))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
